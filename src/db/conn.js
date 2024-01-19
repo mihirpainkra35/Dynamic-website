a0410a046a9d4f1f19662221636efa57a73db692
mihirpainkra35 <96572058+mihirpainkra35@users.noreply.github.com>
2024-01-19 09:48:34 +0530

    connected to mongodb

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 
+//creating a database
 mongoose.connect("mongodb://localhost:27017/dynamic",{
-    useCreateIndex:false,
-    useNewUrlParser:false,
-    useUnifiedTopology:false
 }).then(() => {
-    console.log("connection successfull").catch((error) => {
-        console.log(error);
-    })
+    console.log("connection successfull")
+}).catch((error) => {
+    console.log(error);
 })
