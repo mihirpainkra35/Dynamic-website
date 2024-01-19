2ae52765e75aeed084e93f3be67fbb01172fa77c
mihirpainkra35 <96572058+mihirpainkra35@users.noreply.github.com>
2024-01-19 10:23:02 +0530

    partial folder added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
-require("./db/conn")
+const path = require('path');
+// require("./db/conn");
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
+//setting the path
+const staticpath = path.join(__dirname,"../public");
 
+//middlware
+app.use(express.static(staticpath))
+app.use('/css',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css")));
+app.use('/js',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")));
+app.use('/jq',express.static(path.join(__dirname,"../node_modules/bootstrap/dist")));
+app.set("view engine","hbs");
+
+//routing
 app.get('/',(req,res)=>{
 
-    res.send("hello world");
+    res.render("index");
 
 })
 
